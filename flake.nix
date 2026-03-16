75a024ca60726d4d8ea20fc0d6dd7e86a7e89b34
Tyler Elton <telton007@gmail.com>
2026-03-16 10:19:54 -0400

    feat: init

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Flake for the connect-svelte-query repo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f (import nixpkgs { inherit system; }));
+  in
+  {
+    devShells = forAllSystems (pkgs:
+    {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil
+          nixd
+
+          nodejs_24
+          pnpm
+          typescript-language-server
+        ];
+
+        shellHook = ''
+          echo "Using Node $(node --version)"
+          echo "Using pnpm $(pnpm version)"
+        '';
+      };
+    });
+  };
+}
